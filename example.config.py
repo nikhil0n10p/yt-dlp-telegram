92a3bff61931ded2d563dd7d57d8d1ffac6826b5
nikhil0n10p <nikumar8051@gmail.com>
2025-06-09 11:37:40 +0530

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,3 +1,3 @@
-token = ""  # telegram token
-logs = 2390049  # logs channel id, if none set to None
+token = "8056858217:AAGftmvS51VH-eodNPK0Q0ZXodXGpOPInGA"  # telegram token
+logs = -1002772517385  # logs channel id, if none set to None
 max_filesize = 50000000  # bytes
